53a07f020872fc8a009b8f74f83b6b506cf84d89
Matuti2611 <126129498+Matuti2611@users.noreply.github.com>
2025-09-24 20:29:58 -0300

    Update blockChain.h

--- blockChain.h
+++ blockChain.h
@@ -1,5 +1,19 @@
+#ifndef BLOCKCHAIN_H
+#define BLOCKCHAIN_H
+
+#include "generador_primos.h"
+
 typedef struct NodoBlockchain {
     int id;
     char *mensaje;
     struct NodoBlockchain *anterior;
 } NodoBlockchain;
+
+void actualizar_hoja_y_propagar(ArbolValidacion *arbol, int indice_hoja, long long nuevo_valor);
+int obtener_nuevo_primo(int **lista_primos_ptr, int *cant_primos_usados_ptr, int *cant_primos_totales_ptr);
+void alta(NodoBlockchain **blockchains, ArbolValidacion *arbol_validacion, int blockchain_index, const char *mensaje, int **lista_primos_ptr, int *cant_primos_usados_ptr, int *cant_primos_totales_ptr);
+void actualizar(NodoBlockchain **blockchains, ArbolValidacion *arbol_validacion, int blockchain_index, int id_nodo, const char *nuevo_mensaje, int **lista_primos_ptr, int *cant_primos_usados_ptr, int *cant_primos_totales_ptr);
+int validar(NodoBlockchain **blockchains, ArbolValidacion *arbol_validacion, int num_blockchains);
+int validar_subconjunto(NodoBlockchain **blockchains, int num_blockchains, long long valor_esperado, int min_index, int max_index);
+
+#endif
